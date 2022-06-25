057a9566422bbe225c0bec9ffde9a1b91588c8a1
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-06-25 13:57:47 -0400

    Created Day15K.kt.kt

---
+++ Day15/Day15K.kt
@@ -0,0 +1,29 @@
+package UselessJava.Day15
+
+import java.util.*
+
+class Day15K {
+    fun hasHiddenRickRoll(s: String): Boolean {
+        val test = s.replace("[ \\t\\n]".toRegex(), "")
+        val rickroll = "nevergonnagiveyouup"
+        var regex = ".*"
+        for (i in 0 until rickroll.length) {
+            regex += rickroll[i].toString() + ".*"
+        }
+        return test.lowercase(Locale.getDefault()).matches(Regex(regex))
+    }
+}
+
+fun main(){
+    val d = Day15K()
+    val sc = Scanner(System.`in`)
+    while (true) {
+        println("Enter a string lol")
+        val s = sc.nextLine()
+        if (d.hasHiddenRickRoll(s)) {
+            println("there is a rickroll, be careful")
+        } else {
+            println("no rickroll, you are safe for now")
+        }
+    }
+}
