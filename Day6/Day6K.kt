23ca8c11c662393859e33248ec6cd17dca8cdd54
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-06-25 17:51:21 -0400

    Created Day6K.kt

---
+++ Day6/Day6K.kt
@@ -0,0 +1,55 @@
+package UselessJava.Day6
+
+import java.io.File
+import java.util.*
+
+class Day6K {
+    val dictionary = HashMap<Char, ArrayList<String>>()
+
+    fun Day6(dictionaryFile: String?) {
+        val file = File(dictionaryFile)
+        for (i in 0..25) {
+            dictionary[(i + 'A'.code).toChar()] = ArrayList()
+        }
+        //read a text file and put each word into the dictionary
+        try {
+            val sc = Scanner(file)
+            while (sc.hasNext()) {
+                val s = sc.next()
+                val c = s[0]
+                if (dictionary.containsKey(c)) dictionary[c]!!.add(s)
+            }
+        } catch (e: Exception) {
+            println("Error: " + e.message)
+        }
+    }
+
+    fun createAcronym(s: String): String? {
+        val chars = s.uppercase().toCharArray()
+        var res: String? = ""
+        for (c in chars) {
+            if (dictionary.containsKey(c)) {
+                val strings = dictionary[c]!!
+                res += strings[(Math.random() * strings.size).toInt()] + " "
+            } else res += c
+        }
+        return res
+    }
+}
+
+/*
+   Dictionary file (allWordsInDictionary.txt) from https://github.com/dwyl/english-words.
+   This contains over 450,000 words, so there may be some strange words you may not recognize.
+    */
+var dictionaryFile = "Enter Absolute Path of dictionary file here"
+
+fun main(args: Array<String>) {
+    val d = Day6(dictionaryFile)
+    val sc = Scanner(System.`in`)
+    while (true) {
+        println("Enter a word.")
+        val s = sc.nextLine()
+        println(d.createAcronym(s))
+        println()
+    }
+}
