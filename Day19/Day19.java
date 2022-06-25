d9d7b4101d0be63bf3d1c881116b297f61583e40
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-06-25 07:56:18 -0400

    Created Day 19.

---
+++ Day19/Day19.java
@@ -0,0 +1,38 @@
+package UselessJava.Day19;
+
+import java.util.Scanner;
+
+public class Day19 {
+   
+   final String[] moveArray = "U U2 D D2 L L2 R R2 F F2 B B2 U' D' L' R' F' B' x y z M E S r r2 f f2 b b2 r' f' b'".split(" ");
+   
+   public String createScrambleSequence(int moves) {
+      String res = "";
+      for(int i = 0; i < moves; i++) {
+         res += moveArray[(int)(Math.random() * moveArray.length)] + " ";
+      }
+      return res;
+   }
+   
+   public String createScrambleSequence(String s) {
+      try{
+         int moves = Integer.parseInt(s);
+         return createScrambleSequence(moves);
+      }
+      catch(NumberFormatException e){
+         return Double.NaN + "";
+      }
+   }
+}
+
+class Main{
+   public static void main(String[] args) {
+      Day19 d = new Day19();
+      Scanner sc = new Scanner(System.in);
+      while(true){
+         System.out.println("Enter a number.");
+         String s = sc.nextLine();
+         System.out.println(d.createScrambleSequence(s) + "\n");
+      }
+   }
+}
---
+++ Day19/README.md
@@ -0,0 +1,2 @@
+## Day 19: Scramble sequence generator
+Generates a Rubik's Cube scramble sequence based off of the length of the sequence you give it.
