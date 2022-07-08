4789f4fe8955a3ccbc4d64694932f8d75083e0a4
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-07-07 22:35:51 -0400

    Created Day032.

---
+++ Day032/Day32K.kt
@@ -0,0 +1,45 @@
+package UselessJava.Day032
+
+import java.util.*
+
+
+class Day32K {
+    fun weirdSort(s: String): String? {
+        val strings = s.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val customStrings = arrayOfNulls<CustomString>(strings.size)
+        for (i in strings.indices) {
+            customStrings[i] = CustomString(strings[i])
+        }
+        Arrays.sort(customStrings)
+        return Arrays.toString(customStrings)
+    }
+}
+
+internal class CustomStringK(var s: String) : Comparable<Any?> {
+    var value = 0
+
+    init {
+        for (i in 0 until s.length) {
+            value += s[i].code
+        }
+    }
+
+    override operator fun compareTo(other: Any?): Int {
+        return value - (other as CustomString).value
+    }
+
+    override fun toString(): String {
+        return s
+    }
+}
+
+fun main(){
+    val d = Day32K()
+    val sc = Scanner(System.`in`)
+    while (true) {
+        println("Enter an array of strings.")
+        val s = sc.nextLine()
+        println(d.weirdSort(s))
+    }
+}
+
