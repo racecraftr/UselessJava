3427ee21b2781a61f68e10c36156490929e12452
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-07-13 15:58:16 -0400

    Updated Web files.

--- days/Day015/web/Day15.js
+++ days/Day015/web/Day15.js
@@ -1,9 +1,10 @@
 function foo (a) {
     var test = a.toLowerCase();
+    test = test.replaceAll("\n", "");
     var rickroll = "nevergonnagiveyouup";
-    var regex = "/(.*";
+    var regex = "(.*";
     for(var i = 0; i < rickroll.length; i++){
         regex += rickroll.charAt(i) + ".*";
     }
-    return test.matches(regex + ")/i")
+    return test.matches(regex + ")")
 }
