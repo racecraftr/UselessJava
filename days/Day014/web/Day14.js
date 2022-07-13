1144eebc7acbc1a24214b254b88223cb842f82aa
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-07-13 16:10:40 -0400

    Updated Web files.

--- days/Day014/web/Day14.js
+++ days/Day014/web/Day14.js
@@ -1,5 +1,10 @@
-function foo(bar){
-    var chars = bar.toCharArray();
+function foo(){
+
+    const s = document.getElementById("stringInput").value;
+
+    var chars = s.toCharArray();
     Array.prototype.sort.call(chars);
-    return Array.toString(chars);
+    const res = Array.toString(chars);
+    const output = document.getElementById("output");
+    output.innerHTML = res;
 }
