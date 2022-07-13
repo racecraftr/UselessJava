2b63e32e49c0d91d68d94a7dea418c69b4ee7763
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-07-13 13:49:12 -0400

    Updated Day15.js.

--- days/Day015/web/Day15.js
+++ days/Day015/web/Day15.js
@@ -1,5 +1,5 @@
 function foo (a) {
-    var test = a.replaceAll('([ \t\n])', 'b');
+    var test = a.toLowerCase();
     var rickroll = "nevergonnagiveyouup";
     var regex = "/(.*";
     for(var i = 0; i < rickroll.length; i++){
