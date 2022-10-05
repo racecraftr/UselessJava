03ecc6e7ee9570c62d4c25a7017058cb09824e3f
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-10-04 21:05:28 -0400

    update web files.

--- days/Day006/web/index.js
+++ days/Day006/web/index.js
@@ -2,11 +2,6 @@ const file = "../allWordsInDictionary.txt"
 const wordMap = new Map();
 const A = 65;
 
-function getFile(){
-
-}
-
-
 function init() {
   for (let i = 0; i < 26; i++) {
     wordMap.set(String.fromCharCode(A + i), []);
@@ -26,7 +21,21 @@ function init() {
 }
 
 function foo() {
-  var str = $("#input").val();
-
-  $("#output").html(str);
+  var str = $("#input").val().toUpperCase();
+  var res = "";
+  for (let i = 0; i < str.length; i++) {
+    let element = str.charAt(i);
+    if(wordMap.has(element)){
+      const wordArr = wordMap.get(element);
+      let randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+      res += randomWord.substring(0, 1).toUpperCase() + randomWord.substring(1);
+      res += "<br>"
+    }
+    else{
+        res += element + "<br>";
+    }
+  }
+  res = res.replaceAll(/(\n)/gi, "<br>");
+  console.log(str);
+  $("#output").html(res);
 }
